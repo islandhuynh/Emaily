075594ec28677ede13e30df09e4ad1b6b36016ae
unknown <islandhuynh@gmail.com>
2021-01-28 00:04:37 -0500

    Initiated client folder as a git repository by mistake - attempting to fix it by deleting and re-adding

---
+++ client/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+
+// Note: Browser Router can only take in one child
+
+class App extends Component {
+    componentDidMount () {
+        this.props.fetchUser();
+        console.log('test');
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
+};
+
+// connect assigns actions as props to our App component
+export default connect(null, actions)(App);
