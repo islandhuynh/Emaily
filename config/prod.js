db41ba3d1774b45c11c04af4f3195bcd4da6d0e4
unknown <islandhuynh@gmail.com>
2021-01-28 00:31:11 -0500

    Forgot to removed unwanted line from prod key

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 // prod.js - production keys here
 
-const { stripeSecretKey } = require("./dev");
-
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
