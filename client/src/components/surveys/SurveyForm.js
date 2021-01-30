8658f20342e85a1f0d865f2d2b4151863040c1b0
unknown <islandhuynh@gmail.com>
2021-01-30 18:23:25 -0500

    added validations to all fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     {label: 'Survey Title', name: 'title'},
@@ -45,9 +46,14 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    if (!values.title) {
-        errors.title ='You must provide a title';
-    }
+    // if no emails exist, validate email will take in an empty string - this is because redux form does an initial validation when first run
+    errors.emails = validateEmails(values.emails || '');
+
+    _.each(FIELDS, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = 'You provide a value';
+        }
+    });
 
     return errors;
 }
