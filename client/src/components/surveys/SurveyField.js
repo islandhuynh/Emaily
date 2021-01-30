e22534a987167abaee20456dfdf4c7e1aa2bf933
unknown <islandhuynh@gmail.com>
2021-01-30 17:06:27 -0500

    Added a survey form using Redux Form. Styled the fields. Initialized validation on the subject title field

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+// SurveyField contains logci to render a single label and text input
+import React from 'react';
+
+export default ({ input, label, meta: { error, touched } }) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <input {...input} />
+            {touched && error}
+        </div>
+    );
+};
