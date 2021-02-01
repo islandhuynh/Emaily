77e9142f084d0b7e5091b4438a86d2aa6200b292
unknown <islandhuynh@gmail.com>
2021-02-01 11:33:19 -0500

    Dashboard link fixed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -27,12 +27,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <Link 
-                        to={this.props.user ? '/surveys' : '/'} 
-                        className="left brand-logo"
-                    >
-                        Emaily
-                    </Link>
+                <Link
+                    to={this.props.auth ? '/surveys' : '/'}
+                    className="left brand-logo"
+                >
+                    Emaily
+                </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
